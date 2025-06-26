59da7b5265b5754a2c017ed2ffa19d7068b26a1e
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-06-26 21:47:04 +0200

    Refactor share options styling and toggle functionality for improved layout and interaction

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -2,6 +2,9 @@
 const shareBtn = document.querySelector('.share-button');
 const shareOptions = document.querySelector('.share-options');
 const hideBtn = document.querySelector('.share-options .hidden');
+const shareSVG = document.querySelector('.share');
+
+const footer = document.querySelector('.article-footer');
 
 shareBtn.addEventListener('click', () => {
   shareOptions.classList.toggle('hidden');
@@ -12,4 +15,16 @@ if (hideBtn) {
   hideBtn.addEventListener('click', () => {
     shareOptions.classList.add('hidden');
   });
+}
+
+if (window.innerWidth <= 768) { // Check if we are on mobile size
+  shareSVG.classList.remove('hidden');
+
+  shareBtn.addEventListener('click',()=>{
+    footer.classList.add('hidden')
+  })
+
+  shareSVG.addEventListener('click', () =>{
+    footer.classList.remove('hidden');
+  })
 }
