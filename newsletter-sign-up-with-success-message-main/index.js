5ac89e92715446156a3fb724a27defcf86d45d71
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-07-03 13:36:47 +0200

    Enhance success message functionality and styling in newsletter sign-up form

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -3,6 +3,11 @@ const emailInput = document.getElementById('email');
 const emailErrorMsg = document.getElementById('email-error');
 const form = document.getElementById('form');
 
+const subscribedEmail = document.querySelector('.subscribed-email');
+const container = document.querySelector('article.container');
+const success = document.querySelector('section.success');
+
+const dismiss = document.querySelector('.btn-dismiss');
 
 const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -26,11 +31,23 @@ function handleSubmit(e){
         emailInput.classList.add('error-style');
         console.log('Invalid email!')
     } else {
-        console.log("You are successfully registered!");
+        // console.log("You are successfully registered!");
         // Optionally, submit the form or show a success message
+        container.classList.add('hidden');
+        success.classList.remove('hidden');
+        subscribedEmail.innerText = email;
     }
     
 }
 
+function returnToPage() {
+  success.classList.add('hidden');
+  subscribedEmail.innerText= 'ash@loremcompany.com';
+  container.classList.remove('hidden');
+  emailInput.innerText ='';
+}
 
-form.addEventListener('submit', handleSubmit)
+
+form.addEventListener('submit', handleSubmit);
+
+dismiss.addEventListener('click', returnToPage)
