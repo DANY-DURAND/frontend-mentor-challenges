c918e81f200b2ecace8cffb7b353cc1eca9105e6
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-07-04 23:06:07 +0200

    Add initial JavaScript code for time tracking functionality

---
+++ time-tracking-dashboard-main/index.js
@@ -0,0 +1,10 @@
+const daily = document.querySelector('.daily');
+const weekly = document.querySelector('.weekly');
+const monthly = document.querySelector('.monthly');
+
+const current = document.getElementById('current');
+const currentHours = document.getElementById('current-hours'); 
+
+const timeStamp = document.getElementById('timestamp');
+const previousTime = document.getElementById('previous');
+const hoursTxt = document.getElementById('hours');
