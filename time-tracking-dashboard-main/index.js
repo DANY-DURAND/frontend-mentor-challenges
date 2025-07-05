93c504c821771623583b8c76d99090cedbfa1723
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2025-07-05 23:00:42 +0200

    Add data fetching data

--- time-tracking-dashboard-main/index.js
+++ time-tracking-dashboard-main/index.js
@@ -8,3 +8,16 @@ const currentHours = document.getElementById('current-hours');
 const timeStamp = document.getElementById('timestamp');
 const previousTime = document.getElementById('previous');
 const hoursTxt = document.getElementById('hours');
+
+let data = 'http://127.0.0.1:5501/time-tracking-dashboard-main/data.json'
+
+fetch(data)
+        .then((response)=>{
+            if(!response.ok){
+            console.log('Oops! Something went wrong.');   
+        }
+        return response.json()
+        })
+        .then((data)=>{
+            console.log(data)
+        })
